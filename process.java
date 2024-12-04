6f2ccd2d0b7d5e03604f667735c39c7975fe27b1
codinguniversal <125769338+codinguniversal@users.noreply.github.com>
2024-12-04 02:22:14 +0200

    Update process.java

--- process.java
+++ process.java
@@ -1,14 +1,16 @@
 class Process{
     String name;
+    String color;
     int burstTime;// the time which the process need to complete its execution
     int priority;
-
+    int id;
     int arrivalTime;
     int turnAroundTime;
     int waitingTime;
     int quantum;
-    int remainingBurstTime;
+    int remainingTime;
     int FCAI_factor;
+    int lastExecuteTime;
     
     public Process(String name, int burstTime,int priority){
         this.name=name;
@@ -16,6 +18,17 @@ class Process{
         this.priority=priority;
     
     }
+         // Constructor with default priority value
+         public Process(String name, String color,int id, int arrivalTime, int burstTime) {
+            this.name = name;
+            this.color = color;
+            this.id = id;
+            this.arrivalTime = arrivalTime;
+            this.burstTime = burstTime;
+            this.remainingTime = burstTime;
+            this.priority = Integer.MAX_VALUE;
+            this.lastExecuteTime = arrivalTime;
+        }
 // this consturctor for FCAI algorithm
     public Process(String name, int burstTime,int arrivalTime,int priority,int quantum){
         this.name=name;
@@ -29,7 +42,102 @@ class Process{
     // public void calculateFCAIFactor(double V1, double V2) {
     //     this.FCAI_factor = (10 - this.priority) +(this.arrivalTime / V1) +(this.remainingBurstTime / V2);
     // }
+    //constructor
+    public Process(String name, String color,int id, int arrivalTime, int burstTime, int priority){
+        this.name = name;
+        this.color = color;
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.remainingTime = burstTime;
+        this.priority = priority;
+        this.lastExecuteTime = arrivalTime;
+    }
 
+   
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+    public boolean isCompleted(){
+        return remainingTime <= 0;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+    public int getWaitingTime(){
+        return waitingTime;
+    }
+    public void setWaitingTime(int time){
+        waitingTime = time;
+    }
+    public int getTurnAroundTime(){
+        return turnAroundTime;
+    }
+    public void setTurnAroundTime(int time){
+        turnAroundTime = time;
+    }
+    public int getLastExecuteTime(){
+        return lastExecuteTime;
+    }
+    public void setLastExecuteTime(int time){
+        lastExecuteTime = time;
+    }
+    
+    public void execute() {
+        if (!isCompleted()) {
+            remainingTime--; 
+        }
+    }
 }
+
 
